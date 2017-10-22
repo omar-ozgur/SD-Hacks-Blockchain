e3140720abe433db5fc08452b573fb0b1bbe5cbe
Omar Ozgur <oozgur217@gmail.com>
2017-10-22 08:22:46 -0700

    Added contract upvote functionality

--- hackathon.sol
+++ hackathon.sol
@@ -16,9 +16,12 @@ contract Hackathon {
     string fileHash;
     string workReferences;
     string contributorReferences;
+    uint upvotes;
+    mapping(address => bool) Upvoted;
+    mapping(address => bool) Downvoted;
     address creator;
   }
-  
+
   mapping(address => uint256) public Balances;
   mapping(uint => category) Categories;
   mapping(uint => submission) Submissions;
@@ -73,9 +76,28 @@ contract Hackathon {
     Submissions[submissionID].creator = msg.sender;
   }
 
-  function closeCategory(uint categoryID, uint winnerID) public returns (string status) {
+  function closeCategoryManual(uint categoryID, uint winnerID) public returns (string status) {
     if (block.timestamp < Categories[categoryID].deadline) {
       return;
+    }
+    uint reward = Categories[categoryID].reward;
+    Submissions[winnerID].creator.transfer(reward);
+    Categories[categoryID].winnerID = winnerID;
+    Categories[categoryID].status = "finished";
+    return Categories[categoryID].status;
+  }
+
+  function closeCategoryAutomatic(uint categoryID) public returns (string status) {
+    if (block.timestamp < Categories[categoryID].deadline) {
+      return;
+    }
+    uint highestVotes = 0;
+    uint winnerID = 0;
+    for (uint i = 0; i < numSubmissions; i++) {
+      if (Submissions[i].upvotes > highestVotes) {
+          highestVotes = Submissions[i].upvotes;
+          winnerID = i;
+      }
     }
     uint reward = Categories[categoryID].reward;
     Submissions[winnerID].creator.transfer(reward);
@@ -94,5 +116,25 @@ contract Hackathon {
 
   function getTimestamp() public returns (uint timestamp) {
     return block.timestamp;
+  }
+
+  function upvote(uint submissionID) public {
+    if (Submissions[submissionID].Upvoted[msg.sender]) {
+      return;
+    } else if (Submissions[submissionID].Downvoted[msg.sender]) {
+      Submissions[submissionID].Downvoted[msg.sender] = false;
+    }
+    Submissions[submissionID].Upvoted[msg.sender] = true;
+    Submissions[submissionID].upvotes += 1;
+  }
+
+  function downvote(uint submissionID) public {
+    if (Submissions[submissionID].Downvoted[msg.sender]) {
+      return;
+    } else if (Submissions[submissionID].Upvoted[msg.sender]) {
+      Submissions[submissionID].Upvoted[msg.sender] = false;
+      Submissions[submissionID].upvotes -= 1;
+    }
+    Submissions[submissionID].Downvoted[msg.sender] = true;
   }
 }
